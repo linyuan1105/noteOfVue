c3a14b021143a4228bcebf7ce8d32ec5ec0057eb
yuan.lin <yuan.lin@cyberway.net.cn>
2020-04-30 18:09:19 +0800

    补充数据结构 栈，队列，优先队列

--- src/note/Algorithm/Algorithm.js
+++ src/note/Algorithm/Algorithm.js
@@ -1,19 +1,127 @@
-var isValid = function(s) {
-    let m = {
-        '(':']',
-        '{':')',
-        '[':']'
+//栈类  先进后出
+class Stack{
+    constructor(){
+        this.items=[]
     }
-    let stack = []
-    for(let i=0;i<s.length;i++){
-        if(m[s[i]]){
-            stack.push(s[i])
-        }else if(m[stack.pop()]!=s[i]){
-            return false
+    //入栈
+    push(element){
+        this.items.push(element)
+    }
+    //出栈
+    pop(){
+        return this.items.pop()
+    }
+    //末位
+    peek(){
+        return this.items[this.items.length-1]
+    }
+    //是否为空栈
+    isEmpty(){
+        return !this.items.length
+    }
+    //大小
+    size(){
+        return this.items.length
+    }
+    //清空栈
+    clear(){
+        this.items=[]
+    }
+    //打印栈数据
+    print(){
+        console.log(this.items.toString())
+    }
+}
+
+
+//队列 先进先出
+class Queue{
+    constructor(items){
+        this.items = items||[]
+    }
+    //入队
+    inqueue(element){
+        this.items.push(element)
+    }
+    //出队
+    dequeue(){
+        return this.items.shift()
+    }
+    //队头
+    front(){
+        return this.items[0]
+    }
+    //队尾
+    end(){
+        return this.items[this.items.length-1]
+    }
+    //大小
+    size(){
+        return this.items.length
+    }
+    //是否是空队列
+    isEmpty(){
+        return !this.items.length
+    }
+    //查看队列
+    print(){
+        console.log(this.items.toString())
+    }
+    //置空
+    clear(){
+        this.items=[]
+    }
+}
+
+//优先队列
+class PriorityQueue{
+    constructor(items){
+        this.items =items||[]
+    }
+    inqueue(element,priority){
+        let queueelement = {element,priority}
+        if(this.isEmpty){
+            this.items.push(queueelement)
+        }else{
+            let preindex = this.items.findIndex(item=>{
+                queueelement.priority<item.priority
+            })
+            if(preindex>-1){
+                this.items.splice(preindex,0,queueelement)
+            }else{
+                this.items.push(queueelement)
+            }
         }
     }
-    return stack.length==0
-    
-};
-let ans = isValid("{[]}")
-console.log(ans)
+    //出队
+    dequeue(){
+        return this.items.shift()
+    }
+    //队头
+    front(){
+        return this.items[0]
+    }
+    //队尾
+    end(){
+        return this.items[this.items.length-1]
+    }
+    //大小
+    size(){
+        return this.items.length
+    }
+    //是否是空队列
+    isEmpty(){
+        return !this.items.length
+    }
+    //查看队列
+    print(){
+        this.items.map(item=>{
+            console.log(item)
+        })
+    }
+    //置空
+    clear(){
+        this.items=[]
+    }
+
+}
--- src/note/Algorithm/Algorithm.vue
+++ src/note/Algorithm/Algorithm.vue
@@ -2,36 +2,76 @@
  <div>
      <el-row>
          <el-col :span="6">
-             <h1>冒泡</h1>
+             <h1>冒泡排序</h1>
          </el-col>
-         <el-col :span="18"></el-col>
+         <el-col :span="18">
+            <p>比价任何两个相邻的项，如果第一个比第二个大，则交换他们的位置。
+               交换次数和数组的项有关。
+            </p>
+         </el-col>
      </el-row>
      <el-row>
         <el-col :span="6">
            <h1>选择排序</h1>
         </el-col>
-        <el-col :span="18"></el-col>
+        <el-col :span="18">
+           <p>每一次从待排序的数据元素中选出最小的（或最大）元素，存放在序列的起始位置
+              以此循环，直至排序完毕；
+           </p>
+        </el-col>
      </el-row>
      <el-row>
         <el-col :span="6">
            <h1>插入排序</h1>
         </el-col>
-        <el-col :span="18"></el-col>
+        <el-col :span="18">
+           <p>将一个数据插入到已经排好序的有序数据中，从而得到一个新的，个数加一的有序数据</p>
+        </el-col>
      </el-row>
      <el-row>
         <el-col :span="6">
            <h1>归并排序</h1>
         </el-col>
+        <el-col :span="18">
+           <p>将原始序列切分成较小的序列，直到每个小序列无法再切分。然后执行合并，即将小的序列归并成大的序列
+              ，合并过程进行比较排序，直到最后得到一个排序完毕的大序列。
+           </p>
+        </el-col>
+     </el-row>
+     <el-row>
+        <el-col :span="6">
+         <h1>快速排序</h1>
+        </el-col>
+        <el-col :span="18">
+           <p>通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小
+              ，然后再按此方法对这两部分数据分别进行上述的递归排序，以此达到整个数据变成有序序列。
+           </p>
+        </el-col>
+     </el-row>
+     <el-row>
+        <el-col :span="6">
+         <h1>堆排序</h1>
+        </el-col>
         <el-col :span="18"></el-col>
      </el-row>
      <el-row>
-        <el-col :span="6"></el-col>
-        <el-col :span="18"></el-col>
-     </el-row>
-     <el-row>
-        <el-col :span="6"></el-col>
-        <el-col :span="18"></el-col>
-     </el-row>
+      <el-col :span="6">
+       <h1>希尔排序</h1>
+      </el-col>
+      <el-col :span="18"></el-col>
+   </el-row>
+   <el-row>
+      <el-col :span="6">
+       <h1>桶排序</h1>
+      </el-col>
+      <el-col :span="18"></el-col>
+   </el-row>
+   <el-row>
+      <el-col :span="6">
+       <h1>基数排序</h1>
+      </el-col>
+      <el-col :span="18"></el-col>
+   </el-row>
 
      <el-row>
         <a href="http://caibaojian.com/learn-javascript.html">关于前端的知识点</a>
